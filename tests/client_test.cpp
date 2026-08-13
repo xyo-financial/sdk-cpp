450e86868175f4958764ee98fe373db18ccb27cd
syniol <hadi@syniol.com>
2026-08-13 23:46:42 +0100

    fix(tests): replace valid-syntax URL with malformed URI in test 10k

--- tests/client_test.cpp
+++ tests/client_test.cpp
@@ -853,7 +853,7 @@ int main() {
 
     // 10k. Malformed URL throws validation error
     expects_error(xyo::ErrorCategory::validation, "invalid URL format", [&] {
-      client.downloadEnrichmentCollection("http://api.xyo.financial:99999999/downloads");
+      client.downloadEnrichmentCollection("http://[::1/downloads");
     });
 
     // 10l. Tar header checksum mismatch
