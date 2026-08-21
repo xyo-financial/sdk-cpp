6c2a2a5f8305d1398b19ae10f8d18f1255fe0440
syniol <hadi@syniol.com>
2026-08-21 22:07:02 +0100

    fix(vcpkg): populate sha512 checksum for v2.0.0 release archive

--- packaging/vcpkg/portfile.cmake
+++ packaging/vcpkg/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO xyo-financial/sdk-cpp
     REF "v${VERSION}"
-    SHA512 <SHA512_PLACEHOLDER> # Populated automatically by release workflow
+    SHA512 56b8c7fb253469fd5ac35caf063c368525406e0749c577cdece2beafb91fd59d12f71950b1f56df19261e6fac0406baf15d2653152cd6ef330e4abaab0b66a3f
     HEAD_REF main
 )
 
