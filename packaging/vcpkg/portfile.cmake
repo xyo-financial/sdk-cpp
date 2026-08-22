3389327198007c8ba860be8805afe74fca2cac7b
Hadi Tajallaei <68777073+syniol@users.noreply.github.com>
2026-08-22 08:40:13 +0100

    fix(ci): support local source in vcpkg overlay, set static MSVC CRT in sample, and use Ninja on Windows

--- packaging/vcpkg/portfile.cmake
+++ packaging/vcpkg/portfile.cmake
@@ -1,10 +1,14 @@
-vcpkg_from_github(
-    OUT_SOURCE_PATH SOURCE_PATH
-    REPO xyo-financial/sdk-cpp
-    REF "v${VERSION}"
-    SHA512 6e178bbc8699aa7ec3602c5ff096ffa4b059d9184d14c0dc9c4b4fd3d5c6d17d9d53123b377b1bf3df57fb8afe0565e71636543b58a37703c4df70e2b424bdec
-    HEAD_REF main
-)
+if(EXISTS "${CMAKE_CURRENT_LIST_DIR}/../../CMakeLists.txt")
+  set(SOURCE_PATH "${CMAKE_CURRENT_LIST_DIR}/../..")
+else()
+  vcpkg_from_github(
+      OUT_SOURCE_PATH SOURCE_PATH
+      REPO xyo-financial/sdk-cpp
+      REF "v${VERSION}"
+      SHA512 6e178bbc8699aa7ec3602c5ff096ffa4b059d9184d14c0dc9c4b4fd3d5c6d17d9d53123b377b1bf3df57fb8afe0565e71636543b58a37703c4df70e2b424bdec
+      HEAD_REF main
+  )
+endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
