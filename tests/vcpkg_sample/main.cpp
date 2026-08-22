4a34755a6e45b39dcc395ba76a65671a6fde15d0
syniol <hadi@syniol.com>
2026-08-22 08:05:22 +0100

    fix(include): add forwarding headers and update sample main.cpp

--- tests/vcpkg_sample/main.cpp
+++ tests/vcpkg_sample/main.cpp
@@ -1,9 +1,10 @@
-#include <xyo/xyo_client.hpp>
+#include <xyo/client.hpp>
 #include <iostream>
+#include <utility>
 
 int main() {
     xyo::ClientConfig config("vcpkg_test_token");
-    xyo::XyoClient client(config);
+    xyo::Client client(std::move(config));
     std::cout << "vcpkg consumer built and linked XYO::SDK successfully!\n";
     return 0;
 }
